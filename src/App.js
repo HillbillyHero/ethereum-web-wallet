4a9a81093bdfd51de596972015b12e3f577e7c30
MiikeWilliams <devgator@gmail.com>
2020-11-30 15:38:53 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Web3 from 'web3';
+
+import {
+  GANACHE,
+  SEED_NUMBER_OF_ACCOUNTS,
+} from './constants';
+
+import Navbar from './Navbar';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      wallet: {},
+      account: {},
+    }
+
+    this.ETH_NODE = GANACHE;
+  }
+
+  componentDidMount() {
+    let web3;
+    if (!window.ethereum) {
+      web3 = new Web3(new Web3.providers.HttpProvider(this.ETH_NODE));
+    }
+    const wallet = web3.eth.accounts.wallet.create(
+      SEED_NUMBER_OF_ACCOUNTS, ''
+    );
+    const initialState = this.setInitialState(wallet, wallet[0])
+    this.setState(initialState, () => {
+      console.log(this.state);
+    })
+  }
+
+  setInitialState = (wallet, account) => {
+    return {
+      wallet: wallet,
+      account: account
+    }
+  }
+
+  setWalletState = (wallet) => {
+    return { wallet: wallet };
+  }
+
+  onImportAccount = () => {
+    // import account
+  }
+
+  renderImportAccount = () => {
+    const actionText = 'Import Account';
+    return (
+      <button
+        onClick={this.onImportAccount}
+        className='btn btn-primary'
+      >
+        {actionText}
+      </button>
+    )
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className='container-fluid'>
+          <br />
+          {this.renderImportAccount()}
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// urls
+export const GANACHE = 'http://localhost:7545/';
+
+// web3.eth.accounts.wallet
+export const SEED_NUMBER_OF_ACCOUNTS = 1;
+
