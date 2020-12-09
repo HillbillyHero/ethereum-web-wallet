1266a430deddd92c78a5bb10c38b475b0b19dd63
MiikeWilliams <devgator@gmail.com>
2020-12-09 08:49:30 -0500

    Update ethereum node const to be more generic

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // urls
-export const GANACHE = 'http://localhost:7545/';
+export const ETH_NODE = 'http://localhost:7545/';
 
 // web3.eth.accounts.wallet
 export const DEFAULT_KEY_NAME = "web3js_wallet";
